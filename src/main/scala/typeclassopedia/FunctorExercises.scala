4326ce308a1548eb27fb69aa741df0a1870af98e
Robert Berry <rjberry@gmail.com>
2013-10-06 21:28:04 +0100

    Add Functor instances for Function2, Tuple and Pair.

--- src/main/scala/typeclassopedia/FunctorExercises.scala
+++ src/main/scala/typeclassopedia/FunctorExercises.scala
@@ -3,6 +3,12 @@ package typeclassopedia
 import scalaz._
 
 object FunctorExercises {
+  /** You have to use ({type l[D] = Either[C, D]})# here, as Either[C, _] is not considered by the compiler to be F[_].
+    * This is because of an isomorphism between types and functions in Haskell. All functions in Haskell take one
+    * parameter - multi-parameter functions are generalized under this as functions that take one parameter, then
+    * return other functions. This holds true for types in Haskell, too. Neither is true in Scala by default (although
+    * Scala does offer a syntax for curried functions; this is the closest thing we have for types).
+    */
   implicit def eitherFunctor[C] = new Functor[({type l[D] = Either[C, D]})#l] {
     def map[A, B](fa: Either[C, A])(f: (A) => B): Either[C, B] = fa match {
       case Right(x) => Right(f(x))
@@ -10,5 +16,19 @@ object FunctorExercises {
     }
   }
 
+  implicit def function2Functor[C] = new Functor[({type l[D] = C => D})#l] {
+    def map[A, B](fa: C => A)(f: (A) => B): C => B = f compose fa
+  }
 
+  implicit def tupleFunctor[C] = new Functor[({type l[D] = (C, D)})#l] {
+    def map[A, B](fa: (C, A))(f: (A) => B): (C, B) = (fa._1, f(fa._2))
+  }
+
+  case class Pair[A](a1: A, a2: A)
+
+  implicit def pairFunctor = new Functor[Pair] {
+    def map[A, B](fa: Pair[A])(f: (A) => B): Pair[B] = fa match {
+      case Pair(a1, a2) => Pair(f(a1), f(a2))
+    }
+  }
 }
--- src/test/scala/typeclassopedia/FunctorExercisesSpec.scala
+++ src/test/scala/typeclassopedia/FunctorExercisesSpec.scala
@@ -5,7 +5,7 @@ import FunctorExercises._
 import org.specs2.mutable.Specification
 
 class FunctorExercisesSpec extends Specification {
-  "EitherFunctor" should {
+  "eitherFunctor" should {
     "implement map correctly" in {
       val left: Either[String, Int] = Left("hi")
 
@@ -14,6 +14,32 @@ class FunctorExercisesSpec extends Specification {
       val right: Either[String, Int] = Right(6)
 
       right ∘ { _ * 2 } mustEqual Right(12)
+    }
+  }
+
+  "function2Functor" should {
+    "implement map correctly" in {
+      val f: Int => Int = { _ * 2 }
+
+      val g = f ∘ { _ + 10 }
+
+      g(5) mustEqual 20
+    }
+  }
+
+  "tupleFunctor" should {
+    "implement map correctly" in {
+      val tuple = (5, "hello")
+
+      tuple ∘ { _ + " world" } mustEqual (5, "hello world")
+    }
+  }
+
+  "pairFunctor" should {
+    "implement map correctly" in {
+      val pair = Pair(10, 45)
+
+      pair ∘ { _ / 5 } mustEqual Pair(2, 9)
     }
   }
 }
