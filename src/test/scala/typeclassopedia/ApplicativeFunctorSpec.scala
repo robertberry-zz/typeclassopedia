17bde58c4dd39ab8dd5f647ff4025ab33fab920b
Robert Berry <rjberry@gmail.com>
2013-10-17 21:54:47 +0100

    Add spec for OptionApplicative

--- src/test/scala/typeclassopedia/ApplicativeFunctorSpec.scala
+++ src/test/scala/typeclassopedia/ApplicativeFunctorSpec.scala
@@ -6,14 +6,33 @@ import scalaz._
 import scalaz.Scalaz._
 import ApplicativeFunctorExercises._
 
-object ZipSeqApplicativeSpec extends Properties("ZipSeqFunctor") with FunctionIntIntHelper {
-  property("point") = forAll { (f: Function[Int, Int], xs: List[Int]) =>
+object OptionApplicativeSpec extends Properties("OptionApplicative") with FunctionIntIntHelper {
+  property("point") = forAll { f: Function[Int, Int] =>
+    optionApplicative.point(f) == Some(f)
+  }
+
+  property("<*>") = forAll { (f: Function[Int, Int], x: Int) =>
+    optionApplicative.ap(Some(x))(optionApplicative.point(f)) == Some(f(x)) &&
+    optionApplicative.ap(None)(optionApplicative.point(f)) == None &&
+    optionApplicative.ap(Some(x))(None) == None &&
+    optionApplicative.ap(None)(None) == None
+  }
+}
+
+object ZipSeqApplicativeSpec extends Properties("ZipSeqApplicative") with FunctionIntIntHelper {
+  property("point") = forAll { (f: Function[Int, Int]) =>
+    val pointed = zipStreamApplicative.point(f)
+
+    pointed take 100 forall { _ == f }
+  }
+
+  property("<*>") = forAll { (f: Function[Int, Int], xs: List[Int]) =>
     val seq = ZipStream(xs.toStream)
 
     /* For some reason this only seems to work if I call the applicative typeclass directly. If I try to use implicit
      * resolution it seems to pick up some other typeclass for <*>, causing an infinite loop. Haven't been able to
      * figure out what typeclass or why, though.
-      */
+     */
     zipStreamApplicative.ap(seq)(zipStreamApplicative.point(f)) == xs.map(f)
   }
 }
