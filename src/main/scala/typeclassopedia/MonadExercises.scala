275252382793a4ed02e52880859180d26296a125
Robert Berry <rjberry@gmail.com>
2013-10-26 17:23:21 +0100

    Start Monad exercises

---
+++ src/main/scala/typeclassopedia/MonadExercises.scala
@@ -0,0 +1,37 @@
+package typeclassopedia
+
+import scalaz.{Functor, Monad}
+
+object MonadExercises {
+  implicit object ListMonad extends Monad[List] {
+    def point[A](a: => A): List[A] = List(a)
+
+    def bind[A, B](fa: List[A])(f: (A) => List[B]): List[B] = fa.flatMap(f)
+  }
+
+  implicit def ReaderMonad[C] = new Monad[({type l[A] = Function[C, A]})#l] {
+    def point[A](a: => A): (C) => A = _ => a
+
+    def bind[A, B](fa: (C) => A)(f: (A) => (C) => B): (C) => B = (c: C) => f(fa(c))(c)
+  }
+
+  sealed trait Free[F[_], A]
+  case class Var[F[_], A](a: A) extends Free[F, A]
+  case class Node[F[_], A](f: F[Free[F, A]]) extends Free[F, A]
+
+  implicit def FreeFunctor[F[_]: Functor] = new Functor[({type l[A] = Free[F, A]})#l] {
+    def map[A, B](fa: Free[F, A])(f: (A) => B): Free[F, B] = fa match {
+      case Var(a) => Var(f(a))
+      case Node(fb) => Node(implicitly[Functor[F]].map(fb)(fc => map(fc)(f)))
+    }
+  }
+
+  implicit def FreeMonad[F[_]: Functor] = new Monad[({type l[A] = Free[F, A]})#l] {
+    def point[A](a: => A): Free[F, A] = Var(a)
+
+    def bind[A, B](fa: Free[F, A])(f: (A) => Free[F, B]): Free[F, B] = fa match {
+      case Var(a) => f(a)
+      case Node(fb) => Node(implicitly[Functor[F]].map(fb)(fc => bind(fc)(f)))
+    }
+  }
+}
